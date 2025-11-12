982410897a94284a9b1f7d61e2d0e3feb2687135
MalakMostafa-16003651 <malak.afifi@student.giu-uni.de>
2025-11-12 14:59:42 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -1,15 +1,19 @@
 #ifndef ENCLOSURE_H
 #define ENCLOSURE_H
-
 #include "Animal.h"
-
-//define enclosure class here 
-
-
-
-
-
-
-
-
+#include<iostream>
+using namespace std;
+class Enclosure{
+    private:
+    int capacity;
+    int currentCount;
+    Animal* animal;
+    public:
+    Enclosure();
+    Enclosure(int capacity);
+    void addAnimal(Animal* a);
+    void displayAnimals();
+    ~Enclosure();
+    
+};
 #endif
