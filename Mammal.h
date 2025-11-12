d5124b43cf4e872a8b505239c55703a54a080ae6
MalakMostafa-16003651 <malak.afifi@student.giu-uni.de>
2025-11-12 15:00:10 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -1,13 +1,14 @@
 #ifndef MAMMAL_H
 #define MAMMAL_H
-
 #include "Animal.h"
-
-//define mammal class here
-
-
-
-
-
-
+#include<iostream>
+using namespace std;
+class Mammal:public Animal{
+    private:
+    string furColor;
+    public:
+    Mammal();
+    Mammal(string name, int age, bool isHungry,string furColor);
+    ~Mammal();
+};
 #endif
