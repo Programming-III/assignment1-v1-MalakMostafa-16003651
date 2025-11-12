f8c3afb0955ccdb056b92884324b7bb7f54bacd8
MalakMostafa-16003651 <malak.afifi@student.giu-uni.de>
2025-11-12 15:01:35 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -1,15 +1,15 @@
 #ifndef VISITOR_H
 #define VISITOR_H
-
-#include <string>
+#include<iostream>
 using namespace std;
-
-//define visitor class here
-
-
-
-
-
-
-
+class Visitor{
+    private:
+    string visitorName;
+    int ticketsBought;
+    public:
+    Visitor();
+    Visitor(string visitorName, int ticketsBought);
+    void displayInfo();
+    ~Visitor();
+};
 #endif
