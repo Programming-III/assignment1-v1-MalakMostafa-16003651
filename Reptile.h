bc51197ccff762f025febdebab5727deacd994af
MalakMostafa-16003651 <malak.afifi@student.giu-uni.de>
2025-11-12 15:00:59 +0200

    Define Reptile class inheriting from Animal

--- Reptile.h
+++ Reptile.h
@@ -1,16 +1,15 @@
 #ifndef REPTILE_H
 #define REPTILE_H
-
 #include "Animal.h"
-
-//define reptile class here
-
-
-
-
-
-
-
-
-
+#include<iostream>
+using namespace std;
+class Reptile: public Animal{
+    private:
+    bool isVenomous;
+    public:
+    Reptile();
+    Reptile(string name, int age, bool isHungry,bool isVenomous);
+    void display();
+    ~Reptile();
+};
 #endif
